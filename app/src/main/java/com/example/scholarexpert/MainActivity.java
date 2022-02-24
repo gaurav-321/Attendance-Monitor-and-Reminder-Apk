19c641897cb5662e114a215875867023672a1a9b
Gaurav verma <gauravverma64786@gmail.com>
2022-02-24 15:23:47 +0530

    second day laptop

--- app/src/main/java/com/example/scholarexpert/MainActivity.java
+++ app/src/main/java/com/example/scholarexpert/MainActivity.java
@@ -6,18 +6,25 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.scholarexpert.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-
+    Integer presentLecture = 10;
+    Integer absentLecture = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
         fragmentReplacement(new AttendanceFragment());
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
@@ -36,12 +43,22 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
     }
-
     public void fragmentReplacement(Fragment fragment) {
+
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout2, fragment);
         fragmentTransaction.commit();
 
+    }
+    public void yes_button_click(View view){
+        TextView textbox = findViewById(R.id.textView);
+        presentLecture+=1;
+        textbox.setText(""+presentLecture+"/"+(absentLecture+presentLecture));
+    }
+    public void no_button_click(View view){
+        TextView textbox = findViewById(R.id.textView);
+        absentLecture+=1;
+        textbox.setText(""+presentLecture+"/"+(absentLecture+presentLecture));
     }
 }
