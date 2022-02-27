60a7d15de9ce93b45cf2a7f86f55d912bf1dc349
gaurav verma <gauravverma64786@gmail.com>
2022-02-27 14:39:54 +0530

    2ND WEEK HOME

--- app/src/main/java/com/example/scholarexpert/MainActivity.java
+++ app/src/main/java/com/example/scholarexpert/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     Integer presentLecture = 10;
     Integer absentLecture = 1;
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         fragmentReplacement(new AttendanceFragment());
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
@@ -51,6 +55,39 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
 
     }
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public void all_subject_clicked(View view){
+        Button button1 = findViewById(R.id.all_subject_button);
+        Button button2 = findViewById(R.id.all_subject_button2);
+        button1.setBackground(getResources().getDrawable(R.drawable.atten_button_sel));
+        button1.setTextColor(Color.BLACK);
+        button2.setTextColor(Color.WHITE);
+        button2.setBackground(getResources().getDrawable(R.drawable.atten_button_del));
+
+
+
+    }
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public void timetable_clicked(View view){
+        Button button1 = findViewById(R.id.all_subject_button);
+        Button button2 = findViewById(R.id.all_subject_button2);
+        button1.setBackground(getResources().getDrawable(R.drawable.atten_button_del));
+        button1.setTextColor(Color.WHITE);
+        button2.setTextColor(Color.BLACK);
+        button2.setBackground(getResources().getDrawable(R.drawable.atten_button_sel));
+
+
+
+    }
+    public void add_subject(View view){
+        ProgressBar progress = findViewById(R.id.progressBar);
+        progress.setProgress(progress.getProgress()+1);
+        TextView progresstext = findViewById(R.id.textView4);
+        TextView progresstext2 = findViewById(R.id.textView5);
+        progresstext.setText(progress.getProgress()+"%");
+        progresstext2.setText(progress.getProgress()+"%");
+    }
+    /*
     public void yes_button_click(View view){
         TextView textbox = findViewById(R.id.textView);
         presentLecture+=1;
@@ -61,4 +98,5 @@ public class MainActivity extends AppCompatActivity {
         absentLecture+=1;
         textbox.setText(""+presentLecture+"/"+(absentLecture+presentLecture));
     }
+    */
 }
